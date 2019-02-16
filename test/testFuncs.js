79fe4a65d217f08adf9fc7baae648d832eb1f629
baybird <bayareabird@gmail.com>
2019-02-16 14:35:18 -0800

    New test file for functions class.

---
+++ test/testFuncs.js
@@ -0,0 +1,11 @@
+/*jshint esversion: 6 */
+
+var assert = require('assert');
+// var expect = require('expect');
+
+const config = require("../config.public.js")
+
+describe("Function class", ()=>{
+    var _Func = require("../libs/functions.js");
+
+});
