ab4e5174a16e5c64695080cfc5ed077dbe0f00b9
baybird <bayareabird@gmail.com>
2019-02-16 14:10:52 -0800

    test cases added to validate ip address.

--- test/testIpLookup.js
+++ test/testIpLookup.js
@@ -9,6 +9,39 @@ describe("IP class", ()=>{
     const ipAddress = '202.186.13.4'
     const ipNum = 3401190660;
 
+    describe("Validate an IP number", ()=>{
+        it(`${ipAddress} is a valid IP address.`, (done)=>{
+            var result = _Ip.isAnIpAdress(ipAddress)
+            assert.ok(result)
+            done()
+        })// end it
+
+        var ipAddress2 = '202.186.13'
+        it(`${ipAddress2} is an invalid IP address.`, (done)=>{
+            var result = _Ip.isAnIpAdress(ipAddress2)
+            assert.ok(result != true);
+            done()
+        })// end it
+
+        var ipAddress2 = '202.186.13.256'
+        it(`${ipAddress2} is an invalid IP address.`, (done)=>{
+            var result = _Ip.isAnIpAdress(ipAddress2)
+            assert.ok(result != true);
+            done()
+        })// end it
+    })
+
+    // describe("Validate an IP number", ()=>{
+    //     var ipAddress2 = '202.186.13'
+    //     it(`${ipAddress2} is an invalid IP address.`, (done)=>{
+    //         var result = _Ip.isAnIpAdress(ipAddress2)
+    //         assert.ok(result != true)
+    //         done()
+    //     })// end it
+    // })
+
+
+
     describe("Convert IP address to IP number", ()=>{
         it(`IP Address ${ipAddress} shoud convert to IP number ${ipNum}.`, (done)=>{
             var result = _Ip.address2Number(ipAddress)
@@ -37,40 +70,38 @@ describe("IP class", ()=>{
     //     })// end it
     // })
 
-    before(function(){
-        
-        describe("Load data from CSV file.", ()=>{
-            // this.timeout(5000)            
-    
-            it(`Load data from CSV file.`, (done)=>{
-                let n = 2903459;
+    // describe("Load data from CSV file.", ()=>{
+    //     // this.timeout(5000)            
 
-                _Ip.loadData().then(result=>{
-                    assert.equal(result, n);
-                    done();
-                }).catch(error=>{
-                    done(error);
-                });
-            }); // end it
-        });
-        
-    })
+    //     it(`Load data from CSV file.`, (done)=>{
+    //         let n = 2903459;
 
-    describe("Searching IP", ()=>{
-        var ipAddress = '73.254.88.197';
+    //         _Ip.loadData().then(result=>{
+    //             console.log(result);
+                
+    //             assert.equal(result, n);
+    //             done();
+    //         }).catch(error=>{
+    //             done(error);
+    //         });
+    //     }); // end it
+    // });
 
-        it(`Address should be found for ${ipAddress}`, (done)=>{
-            var item = _Ip.lookupIPAddress(ipAddress)
+    // describe("Searching IP", ()=>{
+    //     var ipAddress = '73.254.88.197';
 
-            if(item){
-                console.log('found: ', item);
-                assert.ok(true)
-                done()
-            }
-            else{
-                done('not found.')           
-            }
-        });// end it
-    });
+    //     it(`Address should be found for ${ipAddress}`, (done)=>{
+    //         var item = _Ip.lookupIPAddress(ipAddress)
+
+    //         if(item){
+    //             console.log('found: ', item);
+    //             assert.ok(true)
+    //             done()
+    //         }
+    //         else{
+    //             done('not found.')           
+    //         }
+    //     });// end it
+    // });
 
 });
